4e8237b832b9d4659317a6e42c04093bebd26c7c
M0rpheusX <maksymilian.matula@gmail.com>
2018-03-18 14:30:41 +0100

    Dodanie klasy kontenera dla Employee

---
+++ ESM/Models/EmployeeList.cs
@@ -0,0 +1,25 @@
+﻿///Klasa kontener dla klasy Employee
+///Jej zadanie to opakowanie w liste objektów klasy Employee
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ESM.Models
+{
+    public class EmployeeList : IEnumerable<Employee>
+    {
+        public List<Employee> Employees { get; set; }
+        public IEnumerator<Employee> GetEnumerator()
+        {
+            return Employees.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
