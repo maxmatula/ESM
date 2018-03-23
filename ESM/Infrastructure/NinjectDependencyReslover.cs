80e8fbefdcfb1c918a62c876d27a2a05d2c2d6cf
M0rpheusX <maksymilian.matula@gmail.com>
2018-03-23 19:26:44 +0100

    Ustawienie strony startowej projektu, dodanie mechanizmu wstrzykiwania zależności część pierwsza

---
+++ ESM/Infrastructure/NinjectDependencyReslover.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace ESM.Infrastructure
+{
+    public class NinjectDependencyReslover : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyReslover(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            //tutaj wrzucamy wszystkie powiązania
+        }
+    }
+}
