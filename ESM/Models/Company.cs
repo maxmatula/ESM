c435a728881733edf11fe79f7305b6f7bb2fc95e
M0rpheusX <maksymilian.matula@gmail.com>
2018-03-29 21:37:35 +0200

    company model

---
+++ ESM/Models/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ESM.Models
+{
+    public class Company
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string Name { get; set; }
+
+        public string Logo { get; set; }
+
+        public string Description { get; set; }
+        public decimal TotalEarnings { get; set; }
+    }
+}
