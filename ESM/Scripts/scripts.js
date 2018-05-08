17c5ff885d6c5b3a8fdbf3091f3a24d24e8f8467
hirashi7 <gwojcik@arkonsoft.pl>
2018-05-08 21:53:59 +0200

    stylowanie i poprawki tłumaczeń

--- ESM/Scripts/scripts.js
+++ ESM/Scripts/scripts.js
@@ -14,13 +14,30 @@
     setInterval(function () {
         $('.main-content').fadeIn("slow");
     }, 100);
-
-    if ($('#particles').lenght) {
+    if ($('#particles').length) {
         /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
         particlesJS.load('particles', '/Scripts/particlesjs-config.json', function () {
             console.log('callback - particles.js config loaded');
         });
     }
-    
+
+    var inputs = document.querySelectorAll('input[type="file"]');
+    Array.prototype.forEach.call(inputs, function (input) {
+        var label = input.nextElementSibling,
+            labelVal = label.innerHTML;
+
+        input.addEventListener('change', function (e) {
+            var fileName = '';
+            if (this.files && this.files.length > 1)
+                fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+            else
+                fileName = e.target.value.split('\\').pop();
+
+            if (fileName)
+                label.querySelector('span').innerHTML = fileName;
+            else
+                label.innerHTML = labelVal;
+        });
+    });
 
 });
