026015883387d6939df002cca577321e96385752
M0rpheusX <maksymilian.matula@gmail.com>
2018-06-01 17:12:21 +0200

    add event aggrement on agreement end

--- ESM/Controllers/AgreementsController.cs
+++ ESM/Controllers/AgreementsController.cs
@@ -12,12 +12,14 @@ namespace ESM.Controllers
     {
 
         private readonly IAgreementsService _agreementService;
+        private readonly IEventsService _eventsService;
         private readonly IDirectoriesService _directoriesService;
 
-        public AgreementsController(IAgreementsService agreementsService, IDirectoriesService directoriesService)
+        public AgreementsController(IAgreementsService agreementsService, IDirectoriesService directoriesService, IEventsService eventsService)
         {
             _agreementService = agreementsService;
             _directoriesService = directoriesService;
+            _eventsService = eventsService;
         }
 
         public ActionResult GetFile(Guid agreementId)
@@ -49,13 +51,23 @@ namespace ESM.Controllers
         {
             if (ModelState.IsValid)
             {
+                if (agreement.EndDate != null)
+                {
+                    Event esmevent = new Event();
+                    string companyId = Session["currentCompanyId"].ToString();
+                    esmevent.EmployeeId = agreement.EmployeeId;
+                    esmevent.CompanyId = Guid.Parse(companyId);
+                    esmevent.Name = agreement.Description;
+                    esmevent.Description = "Koniec umowy o pracę";
+                    esmevent.EventDate = agreement.EndDate.Value;
+                    var resultEvent = _eventsService.CreateEmployeeEvent(esmevent);
+                }
                 var employeeId = agreement.EmployeeId;
                 var userPath = _directoriesService.GetUserDirectory(User.Identity.GetUserId());
                 var filePath = _agreementService.UploadAgreement(userPath, file);
                 var result = _agreementService.SaveAgreementToDb(filePath, agreement);
                 return RedirectToAction("Details", "Employees", new { id = employeeId });
             }
-
             return View(agreement);
         }
     }
